6098fb20a6a2ff26bad259a1b7e2c90c1cba9869
Predrag Pejovic <predragpejovic@Predrags-MacBook-Pro.local>
2025-03-12 13:04:52 +0100

    Ispravka editovanja

--- edit.js
+++ edit.js
@@ -1,105 +1,129 @@
 let rowCounter = 0;
 // Function to handle file upload and load data onto form
 function loadData() {
-    const fileInput = document.getElementById('fileInput');
+  const fileInput = document.getElementById("fileInput");
 
-    // Check if a file is selected
-    if (fileInput.files.length > 0) {
-        const file = fileInput.files[0];
-        const reader = new FileReader();
+  // Check if a file is selected
+  if (fileInput.files.length > 0) {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
 
-        reader.onload = function(event) {
-            const jsonData = event.target.result;
-            const data = JSON.parse(jsonData);
+    reader.onload = function (event) {
+      const jsonData = event.target.result;
+      const data = JSON.parse(jsonData);
 
-            // Extract summary data
-            const summary = data.summary;
+      // Extract summary data
+      const summary = data.summary;
 
-            // Extract individualElements data
-            const individualElements = data.individualElements;
-        
-            // Populate the form with the loaded data
-            populateForm(individualElements, summary, data);
-            rowCounter = individualElements.length +1;
-        };
+      // Extract individualElements data
+      const individualElements = data.individualElements;
 
-        reader.readAsText(file); // Read file as text
-    } else {
-        alert('Molimo izaberite prethodno sacuvani fajl za učitavanje.');
-    }
+      // Populate the form with the loaded data
+      populateForm(individualElements, summary, data);
+      rowCounter = individualElements.length + 1;
+    };
+
+    reader.readAsText(file); // Read file as text
+  } else {
+    alert("Molimo izaberite prethodno sacuvani fajl za učitavanje.");
+  }
 }
 
 // Function to populate the form with individual elements data
 function populateForm(individualElements, summary, data) {
-    const inputsDiv = document.getElementById('inputs');
-    inputsDiv.innerHTML = ''; // Clear existing input rows
+  const inputsDiv = document.getElementById("inputs");
+  inputsDiv.innerHTML = ""; // Clear existing input rows
 
-    // Loop through each element and create input rows
-    individualElements.forEach(element => {
-        const newInputRow = document.createElement('div');
-        newInputRow.className = 'input-row';
-        newInputRow.innerHTML = `
+  // Loop through each element and create input rows
+  individualElements.forEach((element) => {
+    const newInputRow = document.createElement("div");
+    newInputRow.className = "input-row";
+
+    newInputRow.innerHTML = `
             <form>
-                <label for="rowNumber" class="rowNumber">${element.rowNumber}</label>
+                <label for="rowNumber" class="rowNumber">${
+                  element.rowNumber
+                }</label>
                 <label for="name" class="nameLabel">Ime elementa:</label>
-                <input type="text" class="name valueInput" value="${element.name}">
+                <input type="text" class="name valueInput" value="${
+                  element.name
+                }">
                 <label for="length" class="lengthLabel">Duzina (mm):</label>
-                <input type="number" class="length valueInput" value="${element.length}">
+                <input type="number" class="length valueInput" value="${
+                  element.length
+                }">
                 <label for="width" class="widthLabel">Sirina (mm):</label>
-                <input type="number" class="width valueInput" value="${element.width}">
+                <input type="number" class="width valueInput" value="${
+                  element.width
+                }">
                 <label for="result" class="resultLabel">Povrsina (m2):</label>
-                <input type="number" class="result valueInput" value="${element.area}">
+                <input type="number" class="result valueInput" value="${
+                  element.area
+                }"
                 <label for="lengthSide" class="lengthSideLabel">Kant po duzini:</label>
-                <input type="checkbox" class="lengthSideCheckbox valueInput" ${element.lengthSide ? 'checked' : ''}>
-                <input type="checkbox" class="lengthSideCheckbox valueInput" ${element.lengthSide ? 'checked' : ''}>
+                <input type="checkbox" class="lengthSideCheckbox valueInput" ${
+                  element.lengthSide1 ? "checked" : ""
+                }>
+                <input type="checkbox" class="lengthSideCheckbox valueInput" ${
+                  element.lengthSide2 ? "checked" : ""
+                }>
                 <label for="widthSide" class="widthSideLabel">Kant po sirini:</label>
-                <input type="checkbox" class="widthSideCheckbox valueInput" ${element.widthSide ? 'checked' : ''}>
-                <input type="checkbox" class="widthSideCheckbox valueInput" ${element.widthSide ? 'checked' : ''}>
+                <input type="checkbox" class="widthSideCheckbox valueInput" ${
+                  element.widthSide1 ? "checked" : ""
+                }>
+                <input type="checkbox" class="widthSideCheckbox valueInput" ${
+                  element.widthSide2 ? "checked" : ""
+                }>
                 <label for="sideProcessingLength" class="sideProcessingLabel">Duzina kantovanja (m):</label>
-                <input type="number" class="sideProcessingresult valueInput" value="${element.sideProcessingLength}">
+                <input type="number" class="sideProcessingresult valueInput" value="${
+                  element.sideProcessingLength
+                }">
             </form>
         `;
-        inputsDiv.appendChild(newInputRow);
-    });
- let slider = document.getElementById('hideWasteSlider');
- let results = document.querySelector('.end-results');
- results.style.display = "flex";
+    inputsDiv.appendChild(newInputRow);
+  });
+  let slider = document.getElementById("hideWasteSlider");
+  let results = document.querySelector(".end-results");
+  results.style.display = "flex";
 
- let priceOfMaterial = document.querySelector('.priceOfMaterial');
- let priceOfSideProcessing = document.querySelector('.sideProcessingAmount');
- let areaSum = document.querySelector('.sumField');
- let areaSumePlus10 = document.querySelector('.areaPlusWasteField');
- let nandatoryBoardArea = document.querySelector('.mandatoryAreaField');
- let areaSumPlusWaste = document.querySelector('.sumWithWasteField');
- let sideProcessingLength = document.querySelector('.side-proccessing-field');
- let sideProcessingLengthPlusWaste = document.querySelector('.sideProcessingField');
- let totalMaterialPrice = document.querySelector('.sumPriceFiled');
- let totalSideProcessingPrice = document.querySelector('.sideProcessingSum');
- let sumPrice = document.querySelector('.totalAmmount');
+  let priceOfMaterial = document.querySelector(".priceOfMaterial");
+  let priceOfSideProcessing = document.querySelector(".sideProcessingAmount");
+  let areaSum = document.querySelector(".sumField");
+  let areaSumePlus10 = document.querySelector(".areaPlusWasteField");
+  let nandatoryBoardArea = document.querySelector(".mandatoryAreaField");
+  let areaSumPlusWaste = document.querySelector(".sumWithWasteField");
+  let sideProcessingLength = document.querySelector(".side-proccessing-field");
+  let sideProcessingLengthPlusWaste = document.querySelector(
+    ".sideProcessingField"
+  );
+  let totalMaterialPrice = document.querySelector(".sumPriceFiled");
+  let totalSideProcessingPrice = document.querySelector(".sideProcessingSum");
+  let sumPrice = document.querySelector(".totalAmmount");
 
- let sumResult = document.querySelector('.sum-result')
- let sumResultWithWaste = document.querySelector(".sum-result-with-waste");
- let infoText = document.querySelector(".addWasteInfoText");
- let mandatoryArea = document.querySelector(".mandatoryAreaOfBoards");
- let sumAreaPlusWaste = document.querySelector('.areaPlusWaste');
- let saveButton = document.querySelector('.save');
- let writeButton = document.querySelector('.write')
- let saveButtonWholeBoardOption = document.querySelector('.saveWholeBoardOption');
- let writeButtonWholeBoardOption = document.querySelector('.writeWholeBoardOption');
- 
+  let sumResult = document.querySelector(".sum-result");
+  let sumResultWithWaste = document.querySelector(".sum-result-with-waste");
+  let infoText = document.querySelector(".addWasteInfoText");
+  let mandatoryArea = document.querySelector(".mandatoryAreaOfBoards");
+  let sumAreaPlusWaste = document.querySelector(".areaPlusWaste");
+  let saveButton = document.querySelector(".save");
+  let writeButton = document.querySelector(".write");
+  let saveButtonWholeBoardOption = document.querySelector(
+    ".saveWholeBoardOption"
+  );
+  let writeButtonWholeBoardOption = document.querySelector(
+    ".writeWholeBoardOption"
+  );
 
-
- if (data.isWholeBoardMandatory === "Yes"){
-
+  if (data.isWholeBoardMandatory === "Yes") {
     sumResultWithWaste.style.display = "none";
     infoText.style.display = "none";
     sumResult.style.display = "none";
-    saveButton.style.display = 'none'
-    writeButton.style.display = 'none'
-    mandatoryArea.style.display = "flex"
+    saveButton.style.display = "none";
+    writeButton.style.display = "none";
+    mandatoryArea.style.display = "flex";
     sumAreaPlusWaste.style.display = "flex";
-    saveButtonWholeBoardOption.style.display = 'inline-block';
-    writeButtonWholeBoardOption.style.display = 'inline-block';
+    saveButtonWholeBoardOption.style.display = "inline-block";
+    writeButtonWholeBoardOption.style.display = "inline-block";
     slider.value = 1;
 
     priceOfMaterial.value = summary.materialCostPerMeter2;
@@ -107,22 +131,21 @@ function populateForm(individualElements, summary, data) {
     areaSumePlus10.value = summary.areaSum;
     nandatoryBoardArea.value = summary.requiredBoardArea;
     sideProcessingLength.value = summary.totalSideProcessingLength;
-    sideProcessingLengthPlusWaste.value = summary.totalSideProcessingLengthWithWaste;
+    sideProcessingLengthPlusWaste.value =
+      summary.totalSideProcessingLengthWithWaste;
     totalMaterialPrice.value = summary.totalMaterialPrice;
     totalSideProcessingPrice.value = summary.totalSideprocessingPrice;
     sumPrice.value = summary.sumPrice;
-
-
- } else if(data.isWholeBoardMandatory === "No") {
+  } else if (data.isWholeBoardMandatory === "No") {
     priceOfMaterial.value = summary.materialCostPerMeter2;
     priceOfSideProcessing.value = summary.sideProcessingCostPerMeter;
     areaSum.value = summary.areaSum;
     areaSumPlusWaste.value = summary.areaSumWithWaste;
     sideProcessingLength.value = summary.totalSideProcessingLength;
-    sideProcessingLengthPlusWaste.value = summary.totalSideProcessingLengthWithWaste;
+    sideProcessingLengthPlusWaste.value =
+      summary.totalSideProcessingLengthWithWaste;
     totalMaterialPrice.value = summary.totalMaterialPrice;
     totalSideProcessingPrice.value = summary.totalSideprocessingPrice;
     sumPrice.value = summary.sumPrice;
- }
- 
+  }
 }
--- saveAndLoad.js
+++ saveAndLoad.js
@@ -1,123 +1,145 @@
-
 function saveData() {
-    let modulName = prompt("Unesi ime modula");
-    let fileName = prompt("Unesi ime elementa u formatu: projekat/deo projekta");
+  let modulName = prompt("Unesi ime modula");
+  let fileName = prompt("Unesi ime elementa u formatu: projekat/deo projekta");
 
-    // Initialize formData with model name
-    const formData = {
-        modelName: `Model: ${modulName}`,
-        isWholeBoardMandatory :"No",
-        individualElements: [], // Array to hold the data for each row
-        summary: {} // Object to hold the summary data
-    };
+  // Initialize formData with model name
+  const formData = {
+    modelName: `Model: ${modulName}`,
+    isWholeBoardMandatory: "No",
+    individualElements: [], // Array to hold the data for each row
+    summary: {}, // Object to hold the summary data
+  };
 
-    const rows = document.querySelectorAll('.input-row');
+  const rows = document.querySelectorAll(".input-row");
 
-    // Collecting the summary information outside of the loop
-    let materialCost = document.querySelector('.priceOfMaterial').value;
-    let sideProcessingCost = document.querySelector('.sideProcessingAmount').value;
-    let areaSum = document.querySelector('.sumField').value;
-    let areaSumWithWaste = document.querySelector('.sumWithWasteField').value;
-    let sideProcessingLength = document.querySelector('.side-proccessing-field').value;
-    let sideProcessingLengthWithWaste = document.querySelector('.sideProcessingField').value;
-    let totalprice = document.querySelector('.sumPriceFiled').value;
-    let totalSideProcessingPrice = document.querySelector('.sideProcessingSum').value;
-    let sumPrice = document.querySelector('.totalAmmount').value;
+  // Collecting the summary information outside of the loop
+  let materialCost = document.querySelector(".priceOfMaterial").value;
+  let sideProcessingCost = document.querySelector(
+    ".sideProcessingAmount"
+  ).value;
+  let areaSum = document.querySelector(".sumField").value;
+  let areaSumWithWaste = document.querySelector(".sumWithWasteField").value;
+  let sideProcessingLength = document.querySelector(
+    ".side-proccessing-field"
+  ).value;
+  let sideProcessingLengthWithWaste = document.querySelector(
+    ".sideProcessingField"
+  ).value;
+  let totalprice = document.querySelector(".sumPriceFiled").value;
+  let totalSideProcessingPrice =
+    document.querySelector(".sideProcessingSum").value;
+  let sumPrice = document.querySelector(".totalAmmount").value;
 
-    // Assigning summary data once
-    formData.summary = {
-        materialCostPerMeter2: materialCost,
-        sideProcessingCostPerMeter: sideProcessingCost,
-        areaSum: areaSum,
-        areaSumWithWaste: areaSumWithWaste,
-        totalSideProcessingLength: sideProcessingLength,
-        totalSideProcessingLengthWithWaste: sideProcessingLengthWithWaste,
-        totalMaterialPrice: totalprice,
-        totalSideprocessingPrice: totalSideProcessingPrice,
-        sumPrice: sumPrice
-    };
+  // Assigning summary data once
+  formData.summary = {
+    materialCostPerMeter2: materialCost,
+    sideProcessingCostPerMeter: sideProcessingCost,
+    areaSum: areaSum,
+    areaSumWithWaste: areaSumWithWaste,
+    totalSideProcessingLength: sideProcessingLength,
+    totalSideProcessingLengthWithWaste: sideProcessingLengthWithWaste,
+    totalMaterialPrice: totalprice,
+    totalSideprocessingPrice: totalSideProcessingPrice,
+    sumPrice: sumPrice,
+  };
 
-    // Processing each row to extract its data
-    rows.forEach(row => {
-        const rowNumber = row.querySelector('.rowNumber').innerHTML;
-        const nameInput = row.querySelector('.name').value;
-        const lengthInput = row.querySelector('.length').value;
-        const widthInput = row.querySelector('.width').value;
-        const areaResult = row.querySelector('.result').value;
-        const sideLength = row.querySelectorAll('.lengthSideCheckbox');
-        const sideWidth = row.querySelectorAll('.widthSideCheckbox');
-        const sideProcessingResult = row.querySelector('.sideProcessingresult').value;
+  // Processing each row to extract its data
+  rows.forEach((row) => {
+    const rowNumber = row.querySelector(".rowNumber").innerHTML;
+    const nameInput = row.querySelector(".name").value;
+    const lengthInput = row.querySelector(".length").value;
+    const widthInput = row.querySelector(".width").value;
+    const areaResult = row.querySelector(".result").value;
+    const sideLength = row.querySelectorAll(".lengthSideCheckbox");
+    const sideWidth = row.querySelectorAll(".widthSideCheckbox");
+    const sideProcessingResult = row.querySelector(
+      ".sideProcessingresult"
+    ).value;
 
-        let checkedCountLength = 0;
-        sideLength.forEach(checkbox => {
-            if (checkbox.checked) {
-                checkedCountLength++;
-            }
-        });
-        let checkedCountWidth = 0;
-        sideWidth.forEach(checkbox => {
-            if (checkbox.checked) {
-                checkedCountWidth++;
-            }
-        });
-        const rowData = {
-            rowNumber: rowNumber,
-            name: nameInput,
-            length: lengthInput,
-            width: widthInput,
-            area: areaResult,
-            lengthSide: checkedCountLength,
-            widthSide: checkedCountWidth,
-            sideProcessingLength: sideProcessingResult
-        };
-        formData.individualElements.push(rowData); // Push row data into rows array
+    let sideLengthChecked = [];
+    let sideWidthChecked = [];
+
+    let sideLengthCheckedArray = Array.from(sideLength);
+    let sideWidthCheckedArray = Array.from(sideWidth);
+
+    sideLengthCheckedArray.forEach((el) => {
+      sideLengthChecked.push(el.checked);
+    });
+    sideWidthCheckedArray.forEach((el) => {
+      sideWidthChecked.push(el.checked);
     });
 
-    let jsonData = JSON.stringify(formData, null, 2); // Stringify the entire formData object
-    let blob = new Blob([jsonData], { type: "application/json" });
-    let a = document.createElement("a");
-    a.href = window.URL.createObjectURL(blob);
-    a.download = `${fileName}.json`; // Ensure the file has a .json extension
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    let checkedCountLength = 0;
+    sideLength.forEach((checkbox) => {
+      if (checkbox.checked) {
+        checkedCountLength++;
+      }
+    });
+    let checkedCountWidth = 0;
+    sideWidth.forEach((checkbox) => {
+      if (checkbox.checked) {
+        checkedCountWidth++;
+      }
+    });
+
+    const rowData = {
+      rowNumber: rowNumber,
+      name: nameInput,
+      length: lengthInput,
+      width: widthInput,
+      area: areaResult,
+      lengthSide1: sideLengthChecked[0],
+      lengthSide2: sideLengthChecked[1],
+      widthSide1: sideWidthChecked[0],
+      widthSide2: sideWidthChecked[1],
+      sideProcessingLength: sideProcessingResult,
+    };
+    formData.individualElements.push(rowData); // Push row data into rows array
+  });
+
+  let jsonData = JSON.stringify(formData, null, 2); // Stringify the entire formData object
+  let blob = new Blob([jsonData], { type: "application/json" });
+  let a = document.createElement("a");
+  a.href = window.URL.createObjectURL(blob);
+  a.download = `${fileName}.json`; // Ensure the file has a .json extension
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
 
-
-
 function readData() {
-    return new Promise((resolve, reject) => {
-        const fileInput = document.getElementById('fileInput');
-        const file = fileInput.files[0];
+  return new Promise((resolve, reject) => {
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
 
-        if (!file) {
-            reject(new Error('Please select a file.'));
-            return;
-        }
+    if (!file) {
+      reject(new Error("Please select a file."));
+      return;
+    }
 
-        const reader = new FileReader();
+    const reader = new FileReader();
 
-        reader.onload = function(e) {
-            const content = e.target.result;
-            const jsonObject = JSON.parse(content);
-            resolve(jsonObject);
-            const data = {...jsonObject};
-            console.log("Evo data", data)
-        };
+    reader.onload = function (e) {
+      const content = e.target.result;
+      const jsonObject = JSON.parse(content);
+      resolve(jsonObject);
+      const data = { ...jsonObject };
+      console.log("Evo data", data);
+    };
 
-        reader.onerror = function(error) {
-            reject(error);
-        };
+    reader.onerror = function (error) {
+      reject(error);
+    };
 
-        reader.readAsText(file);
-    });
+    reader.readAsText(file);
+  });
 }
 
 async function writeData() {
-try {
+  try {
     const data = await readData();
 
-    let newTab = window.open('', "_blank");
+    let newTab = window.open("", "_blank");
     newTab.document.write(`
     <head>
             <link rel="stylesheet" type="text/css" href="style.css">
@@ -138,7 +160,7 @@ try {
 `);
     // Iterate over each row in the data
     data.individualElements.forEach((row, index) => {
-        newTab.document.write(`
+      newTab.document.write(`
         <tr>
         <td>${index + 1}</td>
         <td>${row.name}</td>
@@ -183,14 +205,7 @@ try {
         <!-- Add more rows as needed -->
     </table>
 `);
-
-
-} catch (error) {
+  } catch (error) {
     console.error(error);
+  }
 }
-}
-
-
-
-
-
